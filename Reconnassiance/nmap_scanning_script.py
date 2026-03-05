76886f0c90c57869e2abefdfc940a7bae21cb8c2
Dylan <haasedylan@gmail.com>
2026-03-04 18:01:48 -0700

    fixed nmap_scanning_script.py so that the scan runs once and now uses subprocess.run to get more info

--- Reconnassiance/nmap_scanning_script.py
+++ Reconnassiance/nmap_scanning_script.py
@@ -5,11 +5,20 @@ def run_nmap():
     scan_type = input("Enter the scan type (for example, -sS, -sV, -A): ")
 
     print("Running nmap...")
-    subprocess.call(["nmap", scan_type, target])
+    
+    result = subprocess.run(
+        ["nmap", scan_type, target],
+        capture_output= True,
+        text = True
+    )
 
     log_file = "nmap_scan_log.txt"
     print(f"Logging scan results to {log_file}...")
-    subprocess.call(["nmap", scan_type, target], stdout=open(log_file, "w"))
+
+    with(log_file, "w") as f:
+        f.write(result.stdout)
+
+    print(f"Results saved to {log_file}")
 
 if __name__ == "__main__":
     run_nmap()
